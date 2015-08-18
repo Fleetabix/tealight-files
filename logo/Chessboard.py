f3547be92e12e644146a3d991a0ed681c7d01974
Fleetabix <Fleetabix@users.noreply.github.com>
2015-08-18 12:05:20 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,9 +1,16 @@
 from tealight.logo import move, turn
-
+def width=200
 def polygon(edges, size):
   angle = 360.0 / edges
   for i in range(0, edges):
     move(size)
     turn(angle)
-    
-polygon(4,200)
+   
+def grid(rows, columns):
+  turn(90)
+  move(width/rows)
+  turn(-90)
+  move(width)
+
+polygon(4,width)
+
