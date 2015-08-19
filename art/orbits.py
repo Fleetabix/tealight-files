5df25cabd66000bbda00404db3d784bbd6771175
Fleetabix <Fleetabix@users.noreply.github.com>
2015-08-19 09:49:26 +0100

    Running art/orbits.py

--- art/orbits.py
+++ art/orbits.py
@@ -43,7 +43,7 @@ def handle_keyup(key):
 
   if key == "left" or key == "right":
     if x<screen_width / 2:
-      ax=-.05
+      ax=.05
     else:
       ax=-.05
   elif key == "up" or key == "down":
