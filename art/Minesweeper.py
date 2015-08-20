5844a38bade92f92a350ae83648205f2bed14989
Fleetabix <Fleetabix@users.noreply.github.com>
2015-08-20 12:29:56 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -1,7 +1,4 @@
 import random
-mines = [[]] * 10 
-
-
   
 def MineGen(MineNumber):
   counter=0
@@ -21,16 +18,33 @@ def NumberGen():
             if mines[i+a][j+b]!=-1:
               mines[i+a][j+b]=mines[i+a][j+b]+1
               
-      
-
+def Extension(x,y):
+  for i in range(-1,1)
+    for i in range (-1,1)
+      if mines[x+i][y+j]==0:
+        uncoverCell(x+i,y+j)
+        Extension(x+i,y+j)        
+      elif mines[x+i][y+j]>0:
+        uncoverCell(x+i,y+j)
+              
+              
+              
+              
+mines = [[]] * 10 
 for i in range(0,10):
-  mines[i] = [0] * 10   
+  mines[i] = [0] * 10 
 
-MineGen(50)
-NumberGen()
-for i in range(0,9):
- for j in range (0,9):
-  print mines[i][j]
+covers = [[]] * 10 
+for i in range(0,10):
+  covers[i] = [0] * 10 
+
+ 
+
+#MineGen(50)
+#NumberGen()
+#for i in range(0,9):
+ #for j in range (0,9):
+  #print mines[i][j]
   
 
 
