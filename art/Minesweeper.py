cb53a57e11a774f1362bdb94112fe5d1368a1dd2
Fleetabix <Fleetabix@users.noreply.github.com>
2015-08-20 12:03:24 +0100

    Running art/Minesweeper.py

--- art/Minesweeper.py
+++ art/Minesweeper.py
@@ -10,18 +10,30 @@ def MineGen(MineNumber):
     tempy=random.randint(0,9)
     print tempx
     print tempy
-    if mines[tempx][tempy]!=1:
-      mines[tempx][tempy]=1
+    if mines[tempx][tempy]!=-1:
+      mines[tempx][tempy]=-1
       counter=counter+1
+  
+def NumberGen():
+  for i in range (0,9):
+    for j in range (0,9):
+      if mines[i][j]==-1:
+        for a in range (-1,1):
+          for b in range (-1,1):
+            if mines[i+a][j+b]==-1:
+            else:
+              mines[i+a][j+b]=mines[i+a][j+b]+1
+              
+      
 
 for i in range(0,10):
   mines[i] = [0] * 10   
 
 MineGen(10)
 
-#for i in range(0,9):
- # for j in range (0,9):
-  #  print mines[i][j]
+for i in range(0,9):
+ for j in range (0,9):
+  print mines[i][j]
   
 
 
